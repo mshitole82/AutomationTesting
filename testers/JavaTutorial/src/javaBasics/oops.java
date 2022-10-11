5c3065dd603a9e2f9a6ff107b07f7d328d9fd53a
mahesh <shitolemb@gmail.com>
2022-10-11 16:17:04 +0530

    first commit adding all files

---
+++ testers/JavaTutorial/src/javaBasics/oops.java
@@ -0,0 +1,32 @@
+package javaBasics;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class oops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+	
+		
+String a="mahesh";
+
+List ex=new ArrayList();
+ex.add(10);
+ex.add(10);
+
+System.out.println(ex.size());
+System.out.println(ex.get(1));
+
+List<Integer> exx = new LinkedList < >();
+
+
+
+
+	}
+
+}
